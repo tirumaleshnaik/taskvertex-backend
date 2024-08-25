fd79e70c3b8b4024d502edaff6b29f07fde7e9f5
admin <admin@admins-MacBook-Pro.local>
2024-08-25 16:07:48 +0530

    Initial commit for project initialization

---
+++ src/main/java/com/tmn/taskvertex_backend/TaskvertexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tmn.taskvertex_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskvertexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskvertexBackendApplication.class, args);
+	}
+
+}
